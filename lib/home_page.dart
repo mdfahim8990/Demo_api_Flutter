32613a9b704ed0db4bc90abf2237fc70f0bd5eea
USER <md899042@gmail.com>
2022-04-25 15:31:05 +0600

    First Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,33 +1,23 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-
-
-
 import 'package:http/http.dart' as http;
-
 class HomePage  extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
-
 var Data;
 var url =Uri.parse (' https://reqres.in/api/users?page=1');
   Future<void> getData()async{
      http.Response response = await http.get(url);
-
     setState(() {
       var decode = json.decode(response.body);
-
       Data = decode;
       print(Data);
     });
   }
-
   @override
   void initState() {
     super.initState();
