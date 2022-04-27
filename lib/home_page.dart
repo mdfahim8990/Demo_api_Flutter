750f52786e4ab00d2cebfcf8b9276cac6a5d8ad7
USER <md899042@gmail.com>
2022-04-27 14:27:43 +0600

    Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,22 +2,27 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-class HomePage  extends StatefulWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
-var Data;
-var url =Uri.parse ('https://reqres.in/api/users?page=1');
-  Future<void> getData()async{
-     http.Response response = await http.get(url);
+  var Data;
+  var url = Uri.parse('https://reqres.in/api/users?page=1');
+
+  Future<void> getData() async {
+    http.Response response = await http.get(url);
     setState(() {
       var decode = json.decode(response.body);
       Data = decode;
       print(Data);
     });
   }
+
   @override
   void initState() {
     super.initState();
@@ -27,12 +32,12 @@ var url =Uri.parse ('https://reqres.in/api/users?page=1');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:_bodyUI(),
+      body: _bodyUI(),
     );
   }
 
-  Widget _bodyUI()=>ListView.builder(
-    itemCount: 30,
-    itemBuilder: (context, index)=>Container(),
-  );
+  Widget _bodyUI() => ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, index) => Container(),
+      );
 }
